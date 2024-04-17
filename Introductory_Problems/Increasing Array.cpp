f4f58d2802225123ec9dadd08162d0c7cf1bbbe0
Tamim Ahmed <166158805+Tamim-Ahmed-15@users.noreply.github.com>
2024-04-18 00:40:24 +0600

    Increasing Array.cpp

--- Introductory_Problems/Increasing Array.cpp
+++ Introductory_Problems/Increasing Array.cpp
@@ -6,21 +6,20 @@ using namespace std;
 #define ll long long
 #define pb push_back
 const int M=1e9+7;
-int vis[1000000];
 int  main()
 {
-	ll n;
+        ll n;
 	cin>>n;
 	vector<ll>v(n);
 	for(ll i=0;i<n;++i)cin>>v[i];
 	ll c=0;
-  for(ll i=0;i<n-1;i++)
-  {
-    if(v[i+1]<v[i])
-    {
+        for(ll i=0;i<n-1;i++)
+        {
+          if(v[i+1]<v[i])
+          {
     		c+=v[i]-v[i+1];
     		v[i+1]=v[i];
-    }
-  }
-    cout<<c;
+          }
+        }
+         cout<<c;
 }
