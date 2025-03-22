c7051d7a547bdc2ebb12a8d06619b4eed6aa4826
Tamim Ahmed <166158805+tamim-ahmed-15@users.noreply.github.com>
2025-03-22 17:55:53 +0600

    Add files via upload

---
+++ Sorting and Searching/Apartments.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define pbc pop_back
+#define pf push_front
+#define pp pop_front
+#define ft first
+#define sd second
+const int N=1000001;
+#define mod 1000000007
+int  main()
+{
+	int n,m,k;
+	cin>>n>>m>>k;
+	vector<int>d;
+	vector<int>a;
+	for(int i=0;i<n;i++){
+		int x; cin>>x;
+		d.pb(x);
+	}
+	for(int i=0;i<m;i++){
+		int x; cin>>x;
+		a.pb(x);
+	}
+	sort(d.begin(),d.end());
+	sort(a.begin(),a.end());
+	int i=0,j=0,c=0;
+	while(i<n && j<m){
+		if(d[i]<(a[j]-k))i++;
+		else if(abs(d[i]-a[j])<=k){
+			c++;
+			i++;
+			j++;
+		}
+		else j++;
+	}
+	cout<<c;
+}
